93b9e0d0132d70376bf08ceddc1cc7205e5f58bb
BuildTools <max.gao@icloud.com>
2015-12-11 13:38:51 +0100

    Initial Commit

---
+++ Burning-Series-tvOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Burning-Series-tvOS
+//
+//  Created by Max Gao on 11.12.15.
+//  Copyright © 2015 Max Gao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
